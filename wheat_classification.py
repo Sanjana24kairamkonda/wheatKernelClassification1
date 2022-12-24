5cee5d715d8bc0d6114f7054e4fac9809681cf0b
Sanjana <kairamkondasanjana3510@gmail.com>
2022-12-24 12:46:07 +0530

    Initial commit

--- wheat_classification.py
+++ wheat_classification.py
@@ -2,15 +2,15 @@ import numpy as np
 from flask import Flask,request,jsonify,render_template
 import pickle
 
-app=Flask(__name__)
+wheat_classification=Flask(__name__)
 
 model=pickle.load(open("model.pkl","rb"))
 
-@app.route("/")
+@wheat_classification.route("/")
 def Home():
     return render_template("index.html")
 
-@app.route("/predict",methods= ["POST"])
+@wheat_classification.route("/predict",methods= ["POST"])
 def predict():
     float_features= [float(x) for x in request.form.values()]
     features=[np.array(float_features)]
@@ -19,4 +19,4 @@ def predict():
     return render_template("index.html", prediction_text = "wheat classification: {}".format(prediction))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    wheat_classification.run(debug=True)
